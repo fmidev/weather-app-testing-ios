51e9236c140afc74cce49076be82da7ed8da51b0
Antti <antti@SIILIM230401.local>
2025-01-24 10:42:07 +0200

    Added more logging and cwd to simulator.

--- variables/config_variables.py
+++ variables/config_variables.py
@@ -7,3 +7,6 @@ CONFIG_APPIUM_SERVER_URL = 'http://127.0.0.1:4723/wd/hub'
 
 # Yarn config variables
 CONFIG_YARN_PATH = '//Users//runner//work//weather-app-testing-ios//weather-app-testing-ios//weather-app'
+
+# Simulator config variables
+CONFIG_SIMULATOR_PATH = '//Users//runner//work//weather-app-testing-ios//weather-app-testing-ios//weather-app'
