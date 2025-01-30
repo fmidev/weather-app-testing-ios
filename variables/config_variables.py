0ed2a17162e9a2e16057941036d71ae2878ff977
Antti <antti@SIILIM230401.local>
2025-01-30 17:10:04 +0200

    Added port logging and added base-path to appium server. Also added udid to capabilities and removed platform version.

--- variables/config_variables.py
+++ variables/config_variables.py
@@ -4,7 +4,7 @@
 #CONFIG_DEVICE_UDID = 'F80CC2E8-BF9A-40DA-A0D0-CF1135D821D1'
 
 # Appium config variables
-CONFIG_APPIUM_SERVER_URL = 'http://127.0.0.1:4723/wd/hub'
+#CONFIG_APPIUM_SERVER_URL = 'http://127.0.0.1:4723/wd/hub'
 
 # Yarn config variables
 CONFIG_YARN_PATH = '//Users//runner//work//weather-app-testing-ios//weather-app-testing-ios//weather-app'
