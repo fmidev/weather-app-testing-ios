e620aaa8fe1b90eb50a057036522fd30f65e636e
Antti <antti@SIILIM230401.local>
2025-01-31 13:24:11 +0200

    Typo fix in onboarding_variables

--- variables/onboarding_variables.py
+++ variables/onboarding_variables.py
@@ -12,5 +12,5 @@ OB_MAP_TEXT_EN = 'For example, you can view the movement of rainfall in the next
 OB_WAR_TEXT_EN = 'Be aware of weather warnings in force in your area.'
 OB_CUS_TEXT_EN = 'In settings, you can select which weather data you want to view and select a dark theme for the app.'
 OB_TOU_TEXT_EN = 'You must accept the terms of use to use the service. Learn more about the Terms of Use by tapping the Terms of Use link.'
-OB_LOC_TEXT_EN = 'The app uses the device location to locate the user. The location is used only for visual display purposes.'
+OB_LOC_TEXT_EN = 'The app uses the device location to locate the user. The location is used only for visual display purposes. '
 
