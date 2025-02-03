d5761dcc06ce28df9b2fa515c97acf5b22649625
Antti <antti@SIILIM230401.local>
2025-02-03 14:51:09 +0200

    More detailed appium server start command. Added CI env var and its check

--- libraries/fmi_custom_library.py
+++ libraries/fmi_custom_library.py
@@ -8,3 +8,10 @@ def get_next_full_hour(time_format = "%H.%M"):
     now = datetime.now()
     next_hour_date = now.replace(microsecond=0, second=0, minute=0) + timedelta(hours=1)
     return next_hour_date.strftime(time_format)
+
+
+""" This function returns True or False if given key exists in given dictionary. """
+
+
+def get_status_is_key_found_in_dictionary(dictionary, key):
+    return key in dictionary
